042f95888a0381519b69212e6f2fe4e80cd7a79e
karnauba <rene.sade@gmail.com>
2022-09-27 14:14:40 +0300

    esimene_homework

---
+++ esimene_homework/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <img className="pilt" src="https://i.kym-cdn.com/photos/images/newsfeed/000/288/625/69c.png" alt="Lebomees"/>
+      <button className="nupp">zuema</button>
+      <div className="text">Miks vanaisa naeratab? Sest vanaisa suitsetas!</div>
+    </div>
+  );
+}
+
+export default App;
